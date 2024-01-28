1fca1ed0715c5cafc68bf99cc41d0d9c2cdaf796
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2024-01-28 18:25:30 +0900

    fix

--- src/main/java/com/example/springdemo/presentation/TaskController.java
+++ src/main/java/com/example/springdemo/presentation/TaskController.java
@@ -17,8 +17,13 @@ import java.util.UUID;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
+@RequestMapping("tasks")
 class TaskController {
     private final TaskCreateUseCase createUseCase;
     private final TaskFindByIdUseCase findByIdUseCase;
